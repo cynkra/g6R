b238221079796b1527dfa380f76f81f9b868355c
schochastics <david@schochastics.net>
2025-06-03 16:10:28 +0200

    added les miserables dataset

---
+++ data-raw/data.R
@@ -0,0 +1,13 @@
+#pak::pak("igraph/igraphdata")
+library(igraph)
+lesmis <- igraphdata::ns_graph("lesmis")
+
+graph_df <- as_data_frame(lesmis, "both")
+nodes <- graph_df$vertices
+names(nodes)[1] <- "id"
+edges <- graph_df$edges
+names(edges)[1:2] <- c("source", "target")
+
+lesmis <- list(nodes = nodes, edges = edges)
+
+usethis::use_data(lesmis, overwrite = TRUE)
