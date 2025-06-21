baa18f4b82c58232d6d4a2d74a38a45e03b76dba
DivadNojnarg <dgranjon@ymail.com>
2025-06-21 22:06:23 +0200

    add combo layout + pokemon data + combo palette example

--- data-raw/data.R
+++ data-raw/data.R
@@ -64,3 +64,49 @@ dag <- jsonlite::fromJSON(
   "https://gw.alipayobjects.com/os/antvdemo/assets/data/algorithm-category.json"
 )
 usethis::use_data(dag, overwrite = TRUE)
+
+# Pokemon data from shinyMons package
+combos <- data.frame(
+  id = paste0("combo-", unique(shinyMons::poke_network$poke_nodes$group))
+)
+combos <- unname(split(
+  combos,
+  seq(nrow(combos))
+))
+
+combos <- lapply(combos, function(combo) {
+  combo <- as.list(combo)
+  combo$data <- list(family = combo$id)
+  combo
+})
+
+nodes <- unname(split(
+  shinyMons::poke_network$poke_nodes,
+  seq(nrow(shinyMons::poke_network$poke_nodes))
+))
+nodes <- lapply(nodes, function(node) {
+  node <- as.list(node)
+  list(
+    id = as.character(node$id),
+    data = list(label = node$label),
+    type = "image",
+    combo = paste0("combo-", as.character(node$group)),
+    style = list(src = node$image)
+  )
+})
+
+edges <- unname(split(
+  shinyMons::poke_network$poke_edges,
+  seq(nrow(shinyMons::poke_network$poke_edges))
+))
+
+edges <- lapply(edges, function(edge) {
+  edge <- as.list(edge)
+  list(
+    source = as.character(edge$from),
+    target = as.character(edge$to)
+  )
+})
+
+poke <- list(nodes = nodes, edges = edges, combos = combos)
+usethis::use_data(poke, overwrite = TRUE)
