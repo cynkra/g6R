b24c4f961e7aad5b9e95506448e567bb8dc0e0eb
DivadNojnarg <dgranjon@ymail.com>
2025-11-19 17:35:22 +0100

    fix dumb mistake

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,8 @@
-shiny::registerInputHandler("g6R.brush_select", function(data, ...) {
-  if (!length(data)) return(NULL)
-  data <- unlist(data)
-  attr(data, "eventType") <- "brush_select"
-  data
-}, force = TRUE)
+.onLoad <- function(...) {
+  shiny::registerInputHandler("g6R.brush_select", function(data, ...) {
+    if (!length(data)) return(NULL)
+    data <- unlist(data)
+    attr(data, "eventType") <- "brush_select"
+    data
+  }, force = TRUE)
+}
